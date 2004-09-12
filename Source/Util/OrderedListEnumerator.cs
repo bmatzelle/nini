094bd742f7d2cdb0ae91f78048a905f2a98e7c68
Brent Matzelle <bmatzelle@gmail.com>
2004-09-12 19:17:44 +0000

    Nini utility class file.

---
+++ Source/Util/OrderedListEnumerator.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+
+namespace Nini.Util
+{
+	/// <include file='OrderedListEnumerator.xml' path='//Class[@name="OrderedListEnumerator"]/docs/*' />
+	public class OrderedListEnumerator : IDictionaryEnumerator
+	{
+		#region Private variables
+		int index = -1;
+		ArrayList list;
+		#endregion
+
+		/// <summary>
+		/// Instantiates an ordered list enumerator with an ArrayList.
+		/// </summary>
+		internal OrderedListEnumerator (ArrayList arrayList)
+		{
+			list = arrayList;
+		}
+
+		#region Public properties
+		/// <include file='OrderedListEnumerator.xml' path='//Property[@name="Current"]/docs/*' />
+		public object Current 
+		{
+			get 
+			{
+				if (index < 0 || index >= list.Count)
+					throw new InvalidOperationException ();
+
+				return list[index];
+			}
+		}
+
+		/// <include file='OrderedListEnumerator.xml' path='//Property[@name="Entry"]/docs/*' />
+		public DictionaryEntry Entry 
+		{
+			get { return (DictionaryEntry) Current; }
+		}
+
+		/// <include file='OrderedListEnumerator.xml' path='//Property[@name="Key"]/docs/*' />
+		public object Key 
+		{
+			get { return Entry.Key; }
+		}
+
+		/// <include file='OrderedListEnumerator.xml' path='//Property[@name="Value"]/docs/*' />
+		public object Value 
+		{
+			get { return Entry.Value; }
+		}
+		#endregion
+
+		#region Public methods
+		/// <include file='OrderedListEnumerator.xml' path='//Method[@name="MoveNext"]/docs/*' />
+		public bool MoveNext ()
+		{
+			index++;
+			if (index >= list.Count)
+				return false;
+
+			return true;
+		}
+
+		/// <include file='OrderedListEnumerator.xml' path='//Method[@name="Reset"]/docs/*' />
+		public void Reset ()
+		{
+			index = -1;
+		}
+		#endregion
+	}
+}
