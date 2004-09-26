947fa61ccb06da7efc31ef75708d880303a4edee
Brent Matzelle <bmatzelle@gmail.com>
2004-09-26 19:46:13 +0000

    ArgvParser: Replaced the StringDictionary with a Hashtable and altered the Regex options to make the class case-sensitive like my original specs.

--- Nini/Source/Util/ArgvParser.cs
+++ Nini/Source/Util/ArgvParser.cs
@@ -11,6 +11,7 @@
 #endregion
 
 using System;
+using System.Collections;
 using System.Collections.Specialized;
 using System.Text.RegularExpressions;
 
@@ -20,46 +21,44 @@ namespace Nini.Util
 	public class ArgvParser
 	{
 		#region Private variables
-		StringDictionary parameters;
+		Hashtable parameters;
 		#endregion
 
 		#region Constructors
 		/// <include file='ArgvParser.xml' path='//Constructor[@name="Constructor"]/docs/*' />
-		public ArgvParser (string[] Args)
+		public ArgvParser (string[] args)
 		{
-			parameters = new StringDictionary ();
-			Regex splitter = new Regex (@"^-{1,2}|^/|=|:", 
-								RegexOptions.IgnoreCase | RegexOptions.Compiled);
-			Regex remover = new Regex (@"^['""]?(.*?)['""]?$", 
-								RegexOptions.IgnoreCase | RegexOptions.Compiled);
+			parameters = new Hashtable ();
+			Regex splitter = new Regex (@"^-{1,2}|^/|=|:", RegexOptions.Compiled);
+			Regex remover = new Regex (@"^['""]?(.*?)['""]?$", RegexOptions.Compiled);
 			string parameter = null;
 			string[] parts;
 
 			// Valid parameters forms:
 			// {-,/,--}param{ ,=,:}((",')value(",'))
 			// Examples: -param1 value1 --param2 /param3:"Test-:-work" /param4=happy -param5 '--=nice=--'
-			foreach (string Text in Args)
+			foreach (string text in args)
 			{
 				// Look for new parameters (-,/ or --) and a possible enclosed value (=,:)
-				parts = splitter.Split (Text, 3);
+				parts = splitter.Split (text, 3);
 				switch (parts.Length)
 				{
 				// Found a value (for the last parameter found (space separator))
 				case 1:
 					if (parameter != null) {
-						if (!parameters.ContainsKey (parameter)) {
+						if (!parameters.Contains (parameter)) {
 							parts[0] = remover.Replace (parts[0], "$1");
 							parameters.Add (parameter, parts[0]);
-							}
-						parameter = null;
 						}
+						parameter = null;
+					}
 					// else Error: no parameter waiting for a value (skipped)
 					break;
 				// Found just a parameter
 				case 2:
 					// The last parameter is still waiting. With no value, set it to true.
 					if (parameter != null) {
-						if (!parameters.ContainsKey (parameter)) {
+						if (!parameters.Contains (parameter)) {
 							parameters.Add (parameter, "true");
 						}
 					}
@@ -69,13 +68,13 @@ namespace Nini.Util
 				case 3:
 					// The last parameter is still waiting. With no value, set it to true.
 					if (parameter != null) {
-						if (!parameters.ContainsKey (parameter)) {
+						if (!parameters.Contains (parameter)) {
 							parameters.Add (parameter, "true");
 						}
 					}
 					parameter = parts[1];
 					// Remove possible enclosing characters  (",')
-					if (!parameters.ContainsKey (parameter)) {
+					if (!parameters.Contains (parameter)) {
 						parts[2] = remover.Replace (parts[2], "$1");
 						parameters.Add (parameter, parts[2]);
 					}
@@ -86,7 +85,7 @@ namespace Nini.Util
 
 			// In case a parameter is still waiting
 			if (parameter !=  null) {
-				if (!parameters.ContainsKey (parameter)) {
+				if (!parameters.Contains (parameter)) {
 					parameters.Add (parameter, "true");
 				}
 			}
@@ -97,7 +96,7 @@ namespace Nini.Util
 		/// <include file='ArgvParser.xml' path='//Property[@name="this"]/docs/*' />
 		public string this [string Param]
 		{
-			get { return (parameters[Param]); }
+			get { return (string)parameters[Param]; }
 		}
 		#endregion
 	}
