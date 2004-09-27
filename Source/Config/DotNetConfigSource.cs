bb61f52e39234b1fdf286103454abf992b272639
Brent Matzelle <bmatzelle@gmail.com>
2004-09-27 21:42:02 +0000

    Made a slew of changes to add true .NET configuration loading support that supports both unit testing and NiniEdit as well.

--- Source/Config/DotNetConfigSource.cs
+++ Source/Config/DotNetConfigSource.cs
@@ -40,8 +40,22 @@ namespace Nini.Config
 		{
 			savePath = ConfigFileName ();
 			configDoc.Load (savePath);
-			this.sections = SectionList (configDoc.DocumentElement);
-			Load ();
+			PerformLoad (configDoc);
+		}
+		
+		/// <include file='DotNetConfigSource.xml' path='//Constructor[@name="ConstructorPath"]/docs/*' />
+		public DotNetConfigSource (string path)
+		{
+			savePath = path;
+			configDoc.Load (savePath);
+			PerformLoad (configDoc);
+		}
+		
+		/// <include file='DotNetConfigSource.xml' path='//Constructor[@name="ConstructorDoc"]/docs/*' />
+		public DotNetConfigSource (XmlDocument document)
+		{
+			configDoc = document;
+			PerformLoad (configDoc);
 		}
 		#endregion
 		
@@ -58,9 +72,8 @@ namespace Nini.Config
 		public void Save ()
 		{
 			if (!IsSavable ()) {
-				throw new Exception ("Source is read only");
+				throw new Exception ("Source cannot be saved in this state");
 			}
-
 			MergeConfigsIntoDocument ();
 		
 			configDoc.Save (savePath);
@@ -93,13 +106,21 @@ namespace Nini.Config
 		/// </summary>
 		private void MergeConfigsIntoDocument ()
 		{
+			RemoveConfigs ();
 			foreach (IConfig config in this.Configs)
 			{
 				string[] keys = config.GetKeys ();
 				
+				string search = "/configuration/" + config.Name;
+				XmlNode node = configDoc.SelectSingleNode (search);
+				if (node == null) {
+					node = SectionNode (config.Name);
+				}
+				RemoveKeys (config.Name);
+				
 				for (int i = 0; i < keys.Length; i++)
 				{
-					SetKey (config.Name, keys[i], config.Get (keys[i]));
+					SetKey (node, keys[i], config.Get (keys[i]));
 				}
 			}
 		}
@@ -119,18 +140,149 @@ namespace Nini.Config
 		}
 		
 		/// <summary>
-		/// Sets an XML key.
+		/// Loads all sections and keys.
 		/// </summary>
-		private void SetKey (string section, string key, string value)
+		private void PerformLoad (XmlDocument doc)
 		{
-			string search = "configuration/" + section 
-							+ "/add[@key='" + key + "']";
+			this.Merge (this); // required for SaveAll
+			configDoc = doc;
 			
-			XmlNode node = configDoc.SelectSingleNode (search);
+			XmlNode rootNode = configDoc.SelectSingleNode ("/configuration");
+			
+			if (rootNode == null) {
+				throw new Exception ("Could not find root node");
+			}
+			
+			LoadSections (rootNode);
+			base.ReplaceTextAll ();
+		}
+		
+		/// <summary>
+		/// Loads all configuration sections.
+		/// </summary>
+		private void LoadSections (XmlNode rootNode)
+		{
+			XmlNodeList nodeList = rootNode.SelectNodes ("configSections/section");
+			ConfigBase config = null;
+			
+			for (int i = 0; i < nodeList.Count; i++)
+			{
+				config = new ConfigBase (nodeList[i].Attributes["name"].Value, this);
+				
+				this.Configs.Add (config);
+				LoadKeys (rootNode, config);
+			}
+			LoadOtherSection (rootNode, "appSettings");
+		}
+		
+		/// <summary>
+		/// Loads special sections that are not loaded in the configSections
+		/// node.  This includes such sections such as appSettings.
+		/// </summary>
+		private void LoadOtherSection (XmlNode rootNode, string nodeName)
+		{
+			XmlNode node = rootNode.SelectSingleNode (nodeName);
+			ConfigBase config = null;
 			
 			if (node != null) {
+				config = new ConfigBase (nodeName, this);
+				
+				this.Configs.Add (config);
+				LoadKeys (rootNode, config);
+			}
+		}
+		
+		/// <summary>
+		/// Loads all keys for a config.
+		/// </summary>
+		private void LoadKeys (XmlNode rootNode, ConfigBase config)
+		{
+			XmlNodeList nodeList = rootNode.SelectNodes (config.Name + "/add");
+
+			for (int i = 0; i < nodeList.Count; i++)
+			{
+				config.Add (nodeList[i].Attributes["key"].Value,
+							nodeList[i].Attributes["value"].Value);
+			}
+		}
+		
+		/// <summary>
+		/// Removes all XML sections that were removed as configs.
+		/// </summary>
+		private void RemoveConfigs ()
+		{
+			XmlAttribute attr = null;
+			XmlNodeList list = configDoc.SelectNodes ("/configuration/configSections/section");
+			
+			// TODO, remove the other section node as well - /configuration/sectionname
+
+			foreach (XmlNode node in list)
+			{
+				attr = node.Attributes["name"];
+				if (attr != null) {
+					if (this.Configs[attr.Value] == null) {
+						node.ParentNode.RemoveChild (node);
+					}
+				} else {
+					throw new Exception ("Section name attribute not found");
+				}
+			}
+		}
+		
+		/// <summary>
+		/// Removes all XML keys that were removed as config keys.
+		/// </summary>
+		private void RemoveKeys (string sectionName)
+		{
+			string search = "/configuration/" + sectionName;
+			XmlNode node = configDoc.SelectSingleNode (search);
+			XmlAttribute keyName = null;
+			
+			if (node != null) {
+				foreach (XmlNode key in node.SelectNodes ("add"))
+				{
+					keyName = key.Attributes["key"];
+					if (keyName != null) {
+						if (this.Configs[sectionName].Get (keyName.Value) == null) {
+							node.RemoveChild (key);
+						}
+					} else {
+						throw new Exception ("Key attribute not found in node");
+					}
+				}
+			}
+		}
+		
+		/// <summary>
+		/// Sets an XML key.  If it does not exist then it is created.
+		/// </summary>
+		private void SetKey (XmlNode sectionNode, string key, string value)
+		{
+			string search = "add[@key='" + key + "']";
+			XmlNode node = sectionNode.SelectSingleNode (search);
+			
+			if (node == null) {
+				CreateKey (sectionNode, key, value);
+			} else {
 				node.Attributes["value"].Value = value;
 			}
+		}
+		
+		/// <summary>
+		/// Creates a key node and adds it to the collection at the end.
+		/// </summary>
+		private void CreateKey (XmlNode sectionNode, string key, string value)
+		{
+			XmlNode node = configDoc.CreateElement ("add");
+			XmlAttribute keyAttr = configDoc.CreateAttribute ("key");
+			XmlAttribute valueAttr = configDoc.CreateAttribute ("value");
+			keyAttr.Value = key;
+			valueAttr.Value = value;
+
+			node.Attributes.Append (keyAttr);
+			node.Attributes.Append (valueAttr);
+
+			sectionNode.AppendChild (node);
 		}
 
 		/// <summary>
@@ -184,11 +336,35 @@ namespace Nini.Config
 		}
 		
 		/// <summary>
+		/// Returns a new section node.
+		/// </summary>
+		private XmlNode SectionNode (string name)
+		{
+			// Add node for configSections node
+			XmlNode node = configDoc.CreateElement ("section");
+			XmlAttribute attr = configDoc.CreateAttribute ("name");
+			attr.Value = name;
+			node.Attributes.Append (attr);
+			
+			attr = configDoc.CreateAttribute ("type");
+			attr.Value = "System.Configuration.NameValueSectionHandler";
+			node.Attributes.Append (attr);
+			string search = "/configuration/configSections"; 
+			configDoc.SelectSingleNode (search).AppendChild (node);
+		
+			// Add node for configuration node
+			XmlNode result = configDoc.CreateElement (name);
+			configDoc.DocumentElement.AppendChild (result);
+			
+			return result;
+		}
+		
+		/// <summary>
 		/// Returns the name of the configuration file for this application.
 		/// </summary>
 		private string ConfigFileName ()
 		{
-			return ((Assembly.GetEntryAssembly()).GetName()).Name +
+			return ((Assembly.GetEntryAssembly ()).GetName ()).Name +
 					".exe.config";
 		}
 		
