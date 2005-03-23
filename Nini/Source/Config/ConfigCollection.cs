38b05a712e02ca1b343756467db74caefd4a1c05
Brent Matzelle <bmatzelle@gmail.com>
2005-03-23 16:35:49 +0000

    ConfigCollection: Now accepts an owner (ConfigSource) input. Added the ConfigAdded and ConfigRemoved events as well as the protected triggers (OnConfigAdded and OnConfigRemoved). Now all add and remove methods trigger the proper events.

--- Nini/Source/Config/ConfigCollection.cs
+++ Nini/Source/Config/ConfigCollection.cs
@@ -13,11 +13,43 @@ using System.Collections;
 
 namespace Nini.Config
 {
+	#region ConfigEventHandler class
+	/// <include file='ConfigEventArgs.xml' path='//Delegate[@name="ConfigEventHandler"]/docs/*' />
+	public delegate void ConfigEventHandler (object sender, ConfigEventArgs e);
+
+	/// <include file='ConfigEventArgs.xml' path='//Class[@name="ConfigEventArgs"]/docs/*' />
+	public class ConfigEventArgs : EventArgs
+	{
+		IConfig config = null;
+
+		/// <include file='ConfigEventArgs.xml' path='//Constructor[@name="ConstructorIConfig"]/docs/*' />
+		public ConfigEventArgs (IConfig config)
+		{
+			this.config = config;
+		}
+
+		/// <include file='ConfigEventArgs.xml' path='//Property[@name="Config"]/docs/*' />
+		public IConfig Config
+		{
+			get { return config; }
+		}
+	}
+	#endregion
+
 	/// <include file='ConfigCollection.xml' path='//Class[@name="ConfigCollection"]/docs/*' />
 	public class ConfigCollection : ICollection, IEnumerable, IList
 	{
 		#region Private variables
 		ArrayList configList = new ArrayList ();
+		ConfigSourceBase owner = null;
+		#endregion
+
+		#region Constructors
+		/// <include file='ConfigCollection.xml' path='//Constructor[@name="Constructor"]/docs/*' />
+		public ConfigCollection (ConfigSourceBase owner)
+		{
+			this.owner = owner;
+		}
 		#endregion
 		
 		#region Public properties
@@ -102,6 +134,7 @@ namespace Nini.Config
 				}
 			} else {
 				configList.Add (config);
+				OnConfigAdded (new ConfigEventArgs (config));
 			}
 		}
 
@@ -118,22 +151,42 @@ namespace Nini.Config
 			}
 		}
 
+		/// <include file='ConfigCollection.xml' path='//Method[@name="AddName"]/docs/*' />
+		public IConfig Add (string name)
+		{
+			ConfigBase result = null;
+
+			if (this[name] == null) {
+				result = new ConfigBase (name, owner);
+				configList.Add (result);
+				OnConfigAdded (new ConfigEventArgs (result));
+			} else {
+				throw new ArgumentException ("An IConfig of that name already exists");
+			}
+			
+			return result;
+		}
+
 		/// <include file='ConfigCollection.xml' path='//Method[@name="Remove"]/docs/*' />
 		public void Remove (IConfig config)
 		{
 			configList.Remove (config);
+			OnConfigRemoved (new ConfigEventArgs (config));
 		}
 
 		/// <include file='ConfigCollection.xml' path='//Method[@name="Remove"]/docs/*' />
 		public void Remove (object config)
 		{
 			configList.Remove (config);
+			OnConfigRemoved (new ConfigEventArgs ((IConfig)config));
 		}
 
 		/// <include file='ConfigCollection.xml' path='//Method[@name="RemoveAt"]/docs/*' />
 		public void RemoveAt (int index)
 		{
+			IConfig config = (IConfig)configList[index];
 			configList.RemoveAt (index);
+			OnConfigRemoved (new ConfigEventArgs (config));
 		}
 
 		/// <include file='ConfigCollection.xml' path='//Method[@name="Clear"]/docs/*' />
@@ -176,6 +229,32 @@ namespace Nini.Config
 		public void Insert (int index, object config)
 		{
 			configList.Insert (index, config);
+		}
+		#endregion
+
+		#region Public events
+		/// <include file='ConfigCollection.xml' path='//Event[@name="ConfigAdded"]/docs/*' />
+		public event ConfigEventHandler ConfigAdded;
+
+		/// <include file='ConfigCollection.xml' path='//Event[@name="ConfigRemoved"]/docs/*' />
+		public event ConfigEventHandler ConfigRemoved;
+		#endregion
+
+		#region Protected methods
+		/// <include file='ConfigCollection.xml' path='//Method[@name="OnConfigAdded"]/docs/*' />
+		protected void OnConfigAdded (ConfigEventArgs e)
+		{
+			if (ConfigAdded != null) {
+				ConfigAdded (this, e);
+			}
+		}
+
+		/// <include file='ConfigCollection.xml' path='//Method[@name="OnConfigRemoved"]/docs/*' />
+		protected void OnConfigRemoved (ConfigEventArgs e)
+		{
+			if (ConfigRemoved != null) {
+				ConfigRemoved (this, e);
+			}
 		}
 		#endregion
 		
